9fdc728e95a506d0d02e10735f9e3d78b0cd32b6
Miguel Lumapat <lamppot@protonmail.com>
2024-06-29 01:06:27 -0400

    Solve P2

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,18 @@ module Main (main) where
 
 import Lib
 
-solutionTo :: Int -> Int
+fibonacci :: [Integer]
+fibonacci = 1 : 2 : zipWith (+) fibonacci (drop 1 fibonacci)
+
+solutionTo :: Int -> Integer
 solutionTo 1 = sum $ filter multipleOfThreeOrFive [3..999]
     where
         multipleOfThreeOrFive n = any (== 0) [n `mod` 3, n `mod` 5]
 
+solutionTo 2 = sum $ filter (\n -> (n `mod` 2) == 0) $ takeWhile (< 4000000) fibonacci
+
+solutionTo _ = 0
+
 
 main :: IO ()
-main = putStrLn $ show $ solutionTo 1
+main = putStrLn $ show $ solutionTo 2
