0a53cd8a08625166e9609c80d276f57f64d6a598
Miguel Lumapat <lamppot@protonmail.com>
2024-06-29 01:51:55 -0400

    Solve P5

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,20 @@ module Main (main) where
 
 import Lib
 
+isMultipleOf :: Integer -> Integer -> Bool
+isMultipleOf a b = (a `mod` b)  == 0
+
+isFactorOf = flip isMultipleOf
+
+-- Initial naive implementation
+primes :: Integer -> [Integer]
+primes 1 = [2]
+primes n = (primes' n (lastPrime + 1)) : lastPrime : remPrimes
+    where
+        (lastPrime : remPrimes) = primes (n - 1)
+        primes' n' i | and (fmap (not . (isMultipleOf i)) (primes (n' - 1))) = i
+                     | otherwise                                             = primes' n' (i+1)
+
 fibonacci :: [Integer]
 fibonacci = 1 : 2 : zipWith (+) fibonacci (drop 1 fibonacci)
 
@@ -12,8 +26,16 @@ solutionTo 1 = sum $ filter multipleOfThreeOrFive [3..999]
 
 solutionTo 2 = sum $ filter (\n -> (n `mod` 2) == 0) $ takeWhile (< 4000000) fibonacci
 
+-- solutionTo 3 = 600851475143
+--     where
+
+solutionTo 5 = head $ filter isSmallestMultiple [2520..]
+    where
+        isSmallestMultiple :: Integer -> Bool
+        isSmallestMultiple n = and $ (isMultipleOf n) <$> [1..20]
+
 solutionTo _ = 0
 
 
 main :: IO ()
-main = putStrLn $ show $ solutionTo 2
+main = putStrLn $ show $ solutionTo 5
