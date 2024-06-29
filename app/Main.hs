1cbc81982ea727a953c56cdda68ca515a5920ca3
Miguel Lumapat <lamppot@protonmail.com>
2024-06-29 00:56:17 -0400

    Solve P1

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,11 @@ module Main (main) where
 
 import Lib
 
+solutionTo :: Int -> Int
+solutionTo 1 = sum $ filter multipleOfThreeOrFive [3..999]
+    where
+        multipleOfThreeOrFive n = any (== 0) [n `mod` 3, n `mod` 5]
+
+
 main :: IO ()
-main = someFunc
+main = putStrLn $ show $ solutionTo 1
