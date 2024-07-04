90f8df3bbde16d0a9fe63f26612af45b5bf53af0
Miguel Lumapat <lamppot@protonmail.com>
2024-07-04 00:27:04 -0400

    Solve P7

--- app/Main.hs
+++ app/Main.hs
@@ -8,13 +8,21 @@ isMultipleOf a b = (a `mod` b)  == 0
 isFactorOf = flip isMultipleOf
 
 -- Initial naive implementation
-primes :: Integer -> [Integer]
-primes 1 = [2]
-primes n = (primes' n (lastPrime + 1)) : lastPrime : remPrimes
+naivePrimes :: Integer -> [Integer]
+naivePrimes 1 = [2]
+naivePrimes n = (primes' n (lastPrime + 1)) : lastPrime : remPrimes
     where
-        (lastPrime : remPrimes) = primes (n - 1)
-        primes' n' i | and (fmap (not . (isMultipleOf i)) (primes (n' - 1))) = i
+        (lastPrime : remPrimes) = naivePrimes (n - 1)
+        primes' n' i | and (fmap (not . (isMultipleOf i)) (naivePrimes (n' - 1))) = i
                      | otherwise                                             = primes' n' (i+1)
+
+primes :: [Integer]
+primes = primes' 2 [] []
+    where
+        primes' :: Integer -> [Integer] -> [Integer] -> [Integer]
+        primes' n [] total = n : primes' (n+1) (n:total) (n:total)
+        primes' n (p:ps) total | n `mod` p > 0 = primes' n ps total
+                               | otherwise     = primes' (n+1) total total
 
 fibonacci :: [Integer]
 fibonacci = 1 : 2 : zipWith (+) fibonacci (drop 1 fibonacci)
@@ -40,8 +48,10 @@ solutionTo 6 = sumSquare - squareSum
         sum' = sum [1..100]
         squareSum = sum' * sum'
 
+solutionTo 7 = primes !! 10000
+
 solutionTo _ = 0
 
 
 main :: IO ()
-main = putStrLn $ show $ solutionTo 6
+main = putStrLn $ show $ solutionTo 7
